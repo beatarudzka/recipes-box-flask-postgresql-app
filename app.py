e82ad09a6d6d9c9d287275b26318e6819dcb4a69
Beata Rudzka <beatarudzka89@gmail.com>
2020-01-27 14:31:54 +0100

    adding templates , template inheritance

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask, escape, request
 
+from flask import Flask, render_template
 app = Flask(__name__)
 
+recipes = [
+    {
+        'author': 'Beata Rudzka',
+        'title': 'Title1',
+        'content': 'Content1',
+        'date_posted': 'January 21 2020'
+    },
+    {
+        'author': 'Beata Rudzka',
+        'title': 'Title2',
+        'content': 'Content2',
+        'date_posted': 'January 21 2020'
+    }
+]
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', recipes=recipes)
+
+
+@app.route("/add")
+def add():
+    return render_template('add.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
