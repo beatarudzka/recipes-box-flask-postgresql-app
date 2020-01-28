7ccc1552ea4015ea2cbacfe9be5844fc8f642b81
Beata Rudzka <beatarudzka89@gmail.com>
2020-01-28 17:10:00 +0100

    more custom styling

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,41 @@
-$(document).ready(function () {
-  $(".menu-icon").on("click", function () {
-    $(".menu_list").toggleClass("showing")
-  })
-})
+$(function () {
 
-$(window).on("scroll", function () {
-  if ($(window).scrollTop()) {
-    $('nav').addClass('black')
-  }
-  else {
-    $('nav').removeClass('black')
-  }
-})
+  // Menu Responsive
+  $(".menu-responsive").toggle();
+
+  $(".menu-bars").on("click", function () {
+    $(".menu-responsive").toggle("slow");
+  });
+
+
+  // Scroll click on menu
+  $(".scroll").on("click", function () {
+    $(this).each(function () {
+      const sectionTop = $(this.hash).offset().top;
+      $("html, body").animate({
+        scrollTop: sectionTop
+      }, 1500);
+    });
+  });
+
+
+  // Progress Bar Animate
+  window.sr = ScrollReveal();
+  sr.reveal(".progress-bar", {
+    origin: "left",
+    duration: 2000,
+    distance: "100%"
+  });
+
+
+  // Contact Text Animate
+  const sentences = ["web designer ?", "web developer ?"];
+  let i = 0;
+
+  setInterval(function () {
+    $(".text-animate").fadeOut(function () {
+      $(this).text(sentences[i = (i + 1) % sentences.length]).fadeIn();
+    });
+  }, 2500);
+
+});
